0a2b96f45af444409140afd0a66edf143544a1a7
omarakamal <omaraakamal@gmail.com>
2025-11-23 19:17:02 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+// get an element in my JS
+// same selector as your css selector
 const mainHeaderElement = document.querySelector('#main-header')
 
 console.log(mainHeaderElement)
+
+
+// changing the text (content) in my element
+mainHeaderElement.textContent = 'Changed Website header'
+
+
+const navLinkElement = document.querySelector('#nav-link')
+
+console.log(navLinkElement)
+
+// to edit ANY attribute use . (dot) notation
+
+navLinkElement.href = 'https://www.google.com'
+
+navLinkElement.style.color = 'red'
+
+navLinkElement.style.backgroundColor = 'purple'
